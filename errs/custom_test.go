c5b63f4df58f54e4be77b52ff9ddf04c92dcacea
maxim <shubinmi@gmail.com>
2020-03-13 13:30:28 +0300

    ~ linter

--- errs/custom_test.go
+++ errs/custom_test.go
@@ -27,7 +27,8 @@ func TestWithState(t *testing.T) {
 			},
 		},
 	}
-	for _, tt := range tests {
+	for _, test := range tests {
+		tt := test
 		t.Run(tt.name, func(t *testing.T) {
 			got := WithState(tt.args.state, tt.args.msg)
 			if !reflect.DeepEqual(got, tt.want) {
